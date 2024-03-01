4be55ba245e01094676ead72d7915c1d2da662e5
Emran Ramezan <Emran@opulence.design>
2024-03-01 00:48:10 +0000

    update

--- Main.java
+++ Main.java
@@ -3,10 +3,22 @@ import Application.Models.CSV.Teacher;
 public class Main {
 
     public static void main(String[] args) {     
-        Teacher teacher = new Teacher("emran","test");
-        teacher.save();
-        teacher.get(1);
-        System.out.println(teacher.getName());
-        teacher.delete(6);
+        // Teacher teacher = new Teacher("emran","test");
+        // Teacher teacher1 = new Teacher("james","test");
+        // Teacher teacher2 = new Teacher("bob","test");
+        // teacher.save();
+        // teacher1.save();
+        // teacher2.save();
+        // teacher.get(1);
+        // System.out.println(teacher.getName());
+        Teacher teacher = new Teacher();
+        Teacher emran = teacher.get(7);
+        Teacher james = teacher.get(12);
+        james.setName("bob");
+        emran.setName("alex");
+        emran.update();
+        james.save();
+
+
     }
 }
