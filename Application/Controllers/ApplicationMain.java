7f160c4eaa9582f634c6b1582f49ae0bac45a700
2805924a@student.gla.ac.uk <73184268+Murad-ILAY@users.noreply.github.com>
2024-03-03 13:40:28 +0000

    initial commit from main to own branch

--- Application/Controllers/ApplicationMain.java
+++ Application/Controllers/ApplicationMain.java
@@ -3,40 +3,40 @@ package Application.Controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import Application.Models.Booking;
+import Application.Models.Requirement;
 import Application.Models.Teacher;
-import Application.Models.CSV.Requirement;
-import Application.Models.CSV.TrainingBooking;
 
 public class ApplicationMain {
 
-    public List<Teacher> teachers;
-    public List<Requirement> requirements;
-    public List<TrainingBooking> bookings;
+	public List<Teacher> teachers;
+	public List<Requirement> requirements;
+	public List<Booking> bookings;
 
-    public ApplicationMain() {
-        teachers = new ArrayList<>();
-        requirements = new ArrayList<>();
-        bookings = new ArrayList<>();
-    }
+	public ApplicationMain() {
+		teachers = new ArrayList<>();
+		requirements = new ArrayList<>();
+		bookings = new ArrayList<>();
+	}
 
-    public List<Teacher> findAvailableTeacher(Requirement req) {
-        return null;
-    }
+	public List<Teacher> findAvailableTeacher(Requirement req) {
+		return null;
+	}
 
-    public void addNewTeacher() {
+	public void addNewTeacher() {
 
-    }
+	}
 
-    public void addNewRequirement() {
+	public void addNewRequirement() {
 
-    }
+	}
 
-    public void assignTeacher(Teacher teacher, Requirement requirement) {
+	public void assignTeacher(Teacher teacher, Requirement requirement) {
 
-    }
+	}
 
-    public void organiseTraining(Teacher teacher) {
+	public void organiseTraining(Teacher teacher) {
 
-    }
+	}
 
 }
