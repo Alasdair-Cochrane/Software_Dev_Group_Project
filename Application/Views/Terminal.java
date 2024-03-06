6905ebf1a985f3b5146b26f0ca8d0d7698c23a04
2805924a@student.gla.ac.uk <73184268+Murad-ILAY@users.noreply.github.com>
2024-03-06 21:14:19 +0000

    update: updated terminal code

--- Application/Views/Terminal.java
+++ Application/Views/Terminal.java
@@ -1,5 +1,30 @@
 package Application.Views;
 
-public class Terminal {
+import java.util.Scanner;
 
+public class Terminal {
+	public static void main(String[] args) {
+		String role;
+		System.out.println(" What is your role: ");
+		Scanner sc = new Scanner(System.in);
+		role = sc.nextLine().toLowerCase();
+		if (role.equals("director")) {
+			menuListForDirector();
+		} else if (role.equals("administrator")) {
+			menuListForAdministrator();
+		}
+
+	}
+
+	private static String menuListForDirector() {
+		return ("Choose operation you want to execute: " + "\n1. Add requirement" + "\n2. View list of all requirement"
+				+ "\n3. View list of all teachers" + "\n4. Add new teacher" + "\n5. List of macthed teachers"
+				+ "\n4. Add new teacher");
+	}
+
+	private static String menuListForAdministrator() {
+		return ("Choose operation you want to execute: " + "\n1. View list of all requirement"
+				+ "\n2. View list of all teachers" + "\n3. Add new teacher" + "\n4. List of macthed teachers"
+				+ "\n5. Add new teacher" + "\n6. Booking training");
+	}
 }
